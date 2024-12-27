a3d819a33bea1b2d39cf8604ae9562110e1f72ac
Nhuphan1602 <nhuphan160200@gmail.com>
2024-12-28 00:50:33 +0700

    setup project structure

---
+++ musify_ios/Musify/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Musify
+//
+//  Created by Nhu Phan on 28/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Home"
+        view.backgroundColor = .red
+    }
+
+
+}
+
