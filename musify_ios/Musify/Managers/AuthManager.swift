5ba2768dfbd011a79b699d8e501580a35c07fb5e
Nhuphan1602 <nhuphan160200@gmail.com>
2025-01-31 00:04:42 +0700

    add signin with webkit feature

--- musify_ios/Musify/Managers/AuthManager.swift
+++ musify_ios/Musify/Managers/AuthManager.swift
@@ -10,7 +10,20 @@ import Foundation
 final class AuthManager {
     static let shared = AuthManager()
     
+    struct Constants {
+        static let clientID = "b8a4de7b933049d89481dc796b242a41"
+        static let clientSecret = "cc73daeabdcd4ce982c818610ca763cd"
+    }
+    
     private init() {}
+    
+    public var signInUrl: URL? {
+        let base = "https://accounts.spotify.com/authorize"
+        let scopes = "user-read-private"
+        let redirectURI = "https://www.iosacademy.io"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        return URL(string: string)
+    }
     
     var isSignedIn: Bool {
         return false
@@ -26,5 +39,17 @@ final class AuthManager {
     
     private var shouldRefreshToken: Bool {
         return false
+    }
+    
+    public func exchangeCodeForToken(code: String, completion: @escaping (Bool) -> Void) {
+        
+    }
+    
+    private func refreshAccessToken() {
+
+    }
+    
+    private func CacheToken() {
+        
     }
 }
--- musify_ios/Musify/Resources/AppDelegate.swift
+++ musify_ios/Musify/Resources/AppDelegate.swift
@@ -12,7 +12,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
         
         let window = UIWindow(frame: UIScreen.main.bounds)
         
